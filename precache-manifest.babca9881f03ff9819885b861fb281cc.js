5b7b00c714760fc819a711a9af79ad0d4aeb5a2f
martovich <mart-1984@mail.ru>
2020-03-03 22:36:24 +0300

    Updates

---
+++ precache-manifest.babca9881f03ff9819885b861fb281cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95332cc7923bc1ee0a07b5c7a31c2e51",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "a9880a37df5c034a1b94",
+    "url": "/todolist/static/css/main.02d8ba0e.chunk.css"
+  },
+  {
+    "revision": "c5620cedd026d6e46669",
+    "url": "/todolist/static/js/2.e62a6c87.chunk.js"
+  },
+  {
+    "revision": "a9880a37df5c034a1b94",
+    "url": "/todolist/static/js/main.c3a6bdb2.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
